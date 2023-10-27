20228be51935517c08c9f8b8836c850e23501fda
antasth <antasth@mail.ru>
2023-10-28 01:18:21 +0300

    feat: add types for MainPage component

--- rsschool-react/src/api/games.ts
+++ rsschool-react/src/api/games.ts
@@ -1,12 +1,5 @@
 import { IGame, IGamesResponseObject } from '../types/types';
 
-// interface ResponseObject<T> {
-//   count: number;
-//   next: string;
-//   previous: string;
-//   results: T[];
-// }
-
 export const getGames = async (page: number): Promise<IGame[]> => {
   const response = await fetch(
     `https://api.rawg.io/api/games?page=${page}&key=83b4eabf3167483aa7ee90180948e7a8`
--- rsschool-react/src/pages/MainPage.tsx
+++ rsschool-react/src/pages/MainPage.tsx
@@ -4,19 +4,18 @@ import { GamesList } from '../components/GamesList/GamesList';
 import { Header } from '../components/Header/Header';
 import { IGame } from '../types/types';
 
-type Games = {
-  gamesList: IGame[];
-};
-
-class MainPage extends Component {
-  state: Games = {
+class MainPage extends Component<
+  Record<string, never>,
+  { gamesList: IGame[] }
+> {
+  state = {
     gamesList: [],
   };
   getGamesList = async (): Promise<void> => {
     this.setState({ gamesList: await getGames(1) });
   };
   componentDidMount(): void {
-    this.getGamesList();
+    // this.getGamesList();
   }
 
   render(): JSX.Element {
