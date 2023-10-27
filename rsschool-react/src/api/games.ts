38eb2c3bfa3344a7926c1140b4a62a7b681ed822
antasth <antasth@mail.ru>
2023-10-27 19:47:00 +0300

    feat: add getGamesList request in MainPage component

--- rsschool-react/src/api/games.ts
+++ rsschool-react/src/api/games.ts
@@ -1,7 +1,18 @@
-export const getGames = async (page: number): Promise<void> => {
+import { IGame, IGamesResponseObject } from '../types/types';
+
+// interface ResponseObject<T> {
+//   count: number;
+//   next: string;
+//   previous: string;
+//   results: T[];
+// }
+
+export const getGames = async (page: number): Promise<IGame[]> => {
   const response = await fetch(
     `https://api.rawg.io/api/games?page=${page}&key=83b4eabf3167483aa7ee90180948e7a8`
   );
-  const games = await response.json();
-  return games;
+  const games: IGamesResponseObject = await response.json();
+  console.log(games);
+
+  return games.results;
 };
--- rsschool-react/src/pages/MainPage.tsx
+++ rsschool-react/src/pages/MainPage.tsx
@@ -1,8 +1,24 @@
 import { Component } from 'react';
+import { getGames } from '../api/games';
 import { GamesList } from '../components/GamesList/GamesList';
 import { Header } from '../components/Header/Header';
+import { IGame } from '../types/types';
+
+type Games = {
+  gamesList: IGame[];
+};
 
 class MainPage extends Component {
+  state: Games = {
+    gamesList: [],
+  };
+  getGamesList = async (): Promise<void> => {
+    this.setState({ gamesList: await getGames(1) });
+  };
+  componentDidMount(): void {
+    this.getGamesList();
+  }
+
   render(): JSX.Element {
     return (
       <>
