86b06a731be04a3feea1c19a4a1224b910228bd7
antasth <antasth@mail.ru>
2023-10-29 23:51:11 +0300

    refactor: remove empty css file

--- rsschool-react/src/components/Loader/Loader.tsx
+++ rsschool-react/src/components/Loader/Loader.tsx
@@ -1,13 +1,8 @@
 import { Component } from 'react';
-import styles from './Loader.module.css';
 
 class Loader extends Component {
   render(): React.ReactElement {
-    return (
-      <div className={styles.loader}>
-        <img src="/svg/loader.svg" alt="loader" />
-      </div>
-    );
+    return <img src="/svg/loader.svg" alt="loader" />;
   }
 }
 
