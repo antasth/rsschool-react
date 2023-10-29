1bd3f3735b75ee754d7490c84873ef64e0b927ca
antasth <antasth@mail.ru>
2023-10-29 19:54:59 +0300

    refactor: remove console.log and comments

--- rsschool-react/src/api/games.ts
+++ rsschool-react/src/api/games.ts
@@ -5,7 +5,6 @@ export const getGames = async (search: string, page = 1): Promise<IGame[]> => {
     `https://api.rawg.io/api/games?page=${page}&search=${search}&key=83b4eabf3167483aa7ee90180948e7a8`
   );
   const games: IGamesResponseObject = await response.json();
-  console.log('getGames', games);
 
   return games.results;
 };
