16a6510a89fb1ad5d4624b13ed2a278a29402804
antasth <antasth@mail.ru>
2023-10-29 17:25:23 +0300

    feat: add svg loader

--- rsschool-react/src/components/Loader/Loader.tsx
+++ rsschool-react/src/components/Loader/Loader.tsx
@@ -3,7 +3,11 @@ import styles from './Loader.module.css';
 
 class Loader extends Component {
   render(): React.ReactElement {
-    return <div className={styles.loader}>Loading...</div>;
+    return (
+      <div className={styles.loader}>
+        <img src="/svg/loader.svg" alt="loader" />
+      </div>
+    );
   }
 }
 
