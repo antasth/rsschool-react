d974ece56ff0b244887ac4a0813e052d9079448e
antasth <antasth@mail.ru>
2023-11-02 00:20:09 +0300

    refactor: change GamesList to functional component

--- rsschool-react/src/components/GamesList/GamesList.tsx
+++ rsschool-react/src/components/GamesList/GamesList.tsx
@@ -1,36 +1,32 @@
-import { Component } from 'react';
+import React from 'react';
 import { IGame } from '../../types/types';
 import styles from './GamesList.module.css';
 
-type PropTypes = {
-  gamesList: IGame[];
-};
-class GamesList extends Component<PropTypes> {
-  render(): React.ReactElement {
-    return (
-      <div className={styles.games}>
-        <h1 className={styles.title}>Games List</h1>
-        <div className={styles.container}>
-          {this.props.gamesList.map((game) => (
-            <div key={game.id} className={styles.card}>
-              <div className={styles.image}>
-                <img
-                  className={styles.img}
-                  src={game.background_image}
-                  alt="game image"
-                />
-              </div>
-              <div className={styles.description}>
-                <h1>{game.name}</h1>
-                <h3>Rating: {game.metacritic}</h3>
-                <h3>released: {game.released}</h3>
-              </div>
+const GamesList = (props: { gamesList: IGame[] }): React.ReactElement => {
+  const { gamesList } = props;
+  return (
+    <div className={styles.games}>
+      <h1 className={styles.title}>Games List</h1>
+      <div className={styles.container}>
+        {gamesList.map((game) => (
+          <div key={game.id} className={styles.card}>
+            <div className={styles.image}>
+              <img
+                className={styles.img}
+                src={game.background_image}
+                alt="game image"
+              />
             </div>
-          ))}
-        </div>
+            <div className={styles.description}>
+              <h1>{game.name}</h1>
+              <h3>Rating: {game.metacritic}</h3>
+              <h3>released: {game.released}</h3>
+            </div>
+          </div>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export { GamesList };
