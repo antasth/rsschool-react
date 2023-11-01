753cdc35a473182b932d671739fb99e5b95f52c1
antasth <antasth@mail.ru>
2023-11-01 20:16:55 +0300

    refactor: change App to functional component

--- rsschool-react/src/App.tsx
+++ rsschool-react/src/App.tsx
@@ -1,11 +1,8 @@
-import { Component } from 'react';
 import './App.css';
 import { MainPage } from './pages/MainPage';
 
-class App extends Component {
-  render(): React.ReactElement {
-    return <MainPage />;
-  }
-}
+const App = (): React.ReactElement => {
+  return <MainPage />;
+};
 
 export { App };
