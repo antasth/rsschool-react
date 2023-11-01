d385736d5f5875a5ab6272dde193da5f9f7b8138
antasth <antasth@mail.ru>
2023-11-01 21:16:54 +0300

    refactor: change Header to functional component

--- rsschool-react/src/components/Header/Header.tsx
+++ rsschool-react/src/components/Header/Header.tsx
@@ -1,45 +1,42 @@
-import { ChangeEvent, Component, FormEvent } from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { getFromLocalStorage, saveToLocalStorage } from '../../utils/utils';
 import styles from './Header.module.css';
 
-type Props = {
+const Header = (props: {
   searchGames: (searchString: string) => void;
-};
-type State = {
-  search: string;
-};
+}): React.ReactElement => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const { searchGames } = props;
 
-class Header extends Component<Props, State> {
-  state: State = { search: getFromLocalStorage() };
+  useEffect(() => {
+    setSearchQuery(getFromLocalStorage());
+  }, []);
 
-  handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ search: e.target.value });
-  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void =>
+    setSearchQuery(e.target.value);
 
-  handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    this.props.searchGames(this.state.search);
-    saveToLocalStorage(this.state.search);
+    searchGames(searchQuery);
+    saveToLocalStorage(searchQuery);
   };
 
-  render(): React.ReactElement {
-    return (
-      <header className={styles.header}>
-        <form className={styles.search} onSubmit={this.handleSubmit}>
-          <input
-            className={styles.input}
-            type="text"
-            placeholder="search..."
-            value={this.state.search}
-            onChange={this.handleChange}
-          />
-          <button className={styles.button} type="submit">
-            Search
-          </button>
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className={styles.header}>
+      <form className={styles.search} onSubmit={handleSubmit}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="search..."
+          value={searchQuery}
+          onChange={handleChange}
+        />
+        <button className={styles.button} type="submit">
+          Search
+        </button>
+      </form>
+    </header>
+  );
+};
 
 export { Header };
