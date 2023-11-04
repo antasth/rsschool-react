373c514e22ea12bb261afa9575c5e35c2aa840be
antasth <antasth@mail.ru>
2023-11-05 01:38:07 +0300

    feat: add Loader to GameDetails

--- rsschool-react/src/components/GameDetails/GameDetails.tsx
+++ rsschool-react/src/components/GameDetails/GameDetails.tsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getGameDetails } from '../../api/games';
 import { IGameDetails } from '../../types/types';
+import { Loader } from '../Loader/Loader';
 import styles from './GameDetails.module.css';
 
 const GameDetails = (): React.ReactElement => {
   const [gameDetails, setGameDetails] = useState<IGameDetails>();
+  const [isLoading, setIsLoading] = useState(true);
 
   const location = useLocation();
 
   const getGame = async (path: string): Promise<void> => {
+    setIsLoading(true);
     const game = await getGameDetails(path);
     setGameDetails(game);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -21,10 +25,16 @@ const GameDetails = (): React.ReactElement => {
   }, [location.pathname]);
 
   return (
-    <>
-      <h1 className={styles.details}>Game Details</h1>
-      {gameDetails?.description}
-    </>
+    <div className={styles.details}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <h1 className={styles.details}>Game Details</h1>
+          <h3>{gameDetails?.description}</h3>
+        </div>
+      )}
+    </div>
   );
 };
 
