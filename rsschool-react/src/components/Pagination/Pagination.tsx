0d5bcacbb5e912d2faf8298b4c8bfe673b04061e
antasth <antasth@mail.ru>
2023-11-04 20:28:48 +0300

    fix: pageSize options values

--- rsschool-react/src/components/Pagination/Pagination.tsx
+++ rsschool-react/src/components/Pagination/Pagination.tsx
@@ -57,10 +57,10 @@ const Pagination = (props: {
         onChange={handlePageSizeChange}
         value={pageSize}
       >
+        <option value={10}>10</option>
         <option value={20}>20</option>
         <option value={30}>30</option>
         <option value={40}>40</option>
-        <option value={50}>50</option>
       </select>
     </div>
   );
