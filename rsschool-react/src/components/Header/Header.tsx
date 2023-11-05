4b2e8994a6cbf1f47080c3bbceb96e105eaf3617
antasth <antasth@mail.ru>
2023-11-05 12:26:35 +0300

    fix: searchQuery save to localstorage

--- rsschool-react/src/components/Header/Header.tsx
+++ rsschool-react/src/components/Header/Header.tsx
@@ -17,7 +17,7 @@ const Header = (props: {
     e.preventDefault();
     setCurrentPage(1);
     setSearchQuery(searchString);
-    saveToLocalStorage(searchQuery);
+    saveToLocalStorage(searchString);
   };
 
   return (
