53d35ac1da4cf3a30712f7c68386c1f71b47ee9f
antasth <antasth@mail.ru>
2023-11-11 14:00:33 +0300

    feat: add Search component tests

--- rsschool-react/src/tests/Search.test.tsx
+++ rsschool-react/src/tests/Search.test.tsx
@@ -1,13 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { Search } from '../components/Search/Search';
+import { getFromLocalStorage } from '../utils';
+
+const GAMES_KEY = 'searchString';
 
 describe('Tests for the Search component:', () => {
-  render(<Search />);
-  const input = screen.getByLabelText('search-input');
+  const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+  const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
 
-  it('Shoud Search component correctly '),
-    (): void => {
-      expect(input).toBeInTheDocument();
-    };
+  afterEach(() => {
+    localStorage.clear();
+    getItemSpy.mockClear();
+    setItemSpy.mockClear();
+  });
+
+  it('Verify that clicking the Search button saves the entered value to the local storage.', (): void => {
+    render(<Search />);
+
+    const button = screen.getByRole('button');
+    const input: HTMLInputElement = screen.getByLabelText('search-input');
+    expect(button).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+
+    fireEvent.change(input, { target: { value: 'some test value' } });
+    fireEvent.click(button);
+
+    expect(getFromLocalStorage(GAMES_KEY)).toEqual(input.value);
+  });
+
+  it('Check that the component retrieves the value from the local storage upon mounting.', (): void => {
+    render(<Search />);
+    const input: HTMLInputElement = screen.getByLabelText('search-input');
+    expect(getFromLocalStorage(GAMES_KEY)).toEqual(input.value);
+  });
 });
