741d0127cb19c469305f46706c0e42804e8d0094
antasth <antasth@mail.ru>
2023-11-18 10:37:58 +0300

    refactor: remove games list from store

--- rsschool-react/src/hooks/useGames.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/store';
-import { IGame } from '../types';
-
-export const useGames = (): { games: IGame[] } => {
-  const games = useSelector((state: RootState) => state.games.value);
-
-  return { games };
-};
--- rsschool-react/src/store/store.ts
+++ rsschool-react/src/store/store.ts
@@ -1,12 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { api } from './api/api';
-import { reducer as gamesReducer } from './games/games.slice';
 import { reducer as pageSizeReducer } from './pageSize/pageSize.slice';
 import { reducer as searchReducer } from './search/search.slice';
 
 const reducers = combineReducers({
   search: searchReducer,
-  games: gamesReducer,
   pageSize: pageSizeReducer,
   [api.reducerPath]: api.reducer,
 });
